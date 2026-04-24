856709559820b03dad95956d7e29a3597a75695a
Mart <mart.zielman@kpnmail.nl>
2026-04-24 17:29:54 +0200

    feat: display actual data on frontend

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -7,6 +7,10 @@
     let selected = $state(null);
     let open = $state(false);
 
+    let averageDelay = $state("No data available");
+    let trainsOnTime = $state("No data available");
+    let onTimePercentage = $state("No data available");
+
     async function complete(q: string) {
         if (q.length < 2) {
             results = [];
@@ -20,6 +24,14 @@
         selected = feed;
         query = feed.feed_name;
         open = false;
+    }
+
+    async function updateStats(feed: any) {
+        const delays = await getDelays(feed).then((data) => {
+            averageDelay = data.realtime.avg_delay;
+            trainsOnTime = data.realtime.on_time;
+            onTimePercentage = data.realtime.on_time_percentage;
+        });
     }
 </script>
 
@@ -74,11 +86,13 @@
                                 class="px-3 py-2 hover:bg-gray-100 cursor-pointer flex flex-col"
                                 onmousedown={() => {
                                     select(feed);
-                                    console.log(getDelays(feed));
+                                    updateStats(feed);
                                 }}
                                 onkeydown={(e) => {
-                                    if (e.key === "Enter" || e.key === " ")
+                                    if (e.key === "Enter" || e.key === " ") {
                                         select(feed);
+                                        updateStats(feed);
+                                    }
                                 }}
                                 role="option"
                                 aria-selected={selected === feed}
@@ -100,43 +114,53 @@
             </div>
         </div>
     </div>
-    <div class="mt-5">
-        <p class="text-sm font-display text-gray-500">
-            * Realtime GTFS data may be less accurate or nonexistent in places.<br
-            />
-            How to use: Type in the full country or provider name and wait for the
-            feeds to load (this may take up to 30 seconds due to API limitations).
-            Then choose the data you want to see.
-        </p>
-    </div>
-    <div class="flex flex-col md:flex-row mt-10 gap-10">
-        <div class="flex flex-col max-w-xs">
-            <p class="text-green-600 font-display font-bold text-5xl">40.7</p>
-            <p class="italic font-serif">seconds average delay right now</p>
-            <hr class="my-2 h-0.5 border-t-0 bg-gray-400" />
-            <p class="font-serif text-md">
-                Not a bad score for 5:34PM, rush hour! That's less than a minute
-                for the most crowded part of the day!
+    {#if selected}
+        <div class="mt-5">
+            <p class="text-sm font-display text-gray-500">
+                * Realtime GTFS data may be less accurate or nonexistent in
+                places.<br />
+                How to use: Type in the full country or provider name and wait for
+                the feeds to load (this may take up to 30 seconds due to API limitations).
+                Then choose the data you want to see.
             </p>
         </div>
-        <div class="flex flex-col max-w-xs">
-            <p class="text-green-600 font-display font-bold text-5xl">5421</p>
-            <p class="italic font-serif">trains on time</p>
-            <hr class="my-2 h-0.5 border-t-0 bg-gray-400" />
-            <p class="font-serif text-md">
-                In your country, there's an average of 324 people on a train.
-                This means 1.8 million people are transported to their
-                destination without friction right now!
-            </p>
+        <div class="flex flex-col md:flex-row mt-10 gap-10">
+            <div class="flex flex-col max-w-xs">
+                <p class="text-green-600 font-display font-bold text-5xl">
+                    {Math.round(averageDelay * 10) / 10}
+                </p>
+                <p class="italic font-serif">seconds average delay right now</p>
+                <hr class="my-2 h-0.5 border-t-0 bg-gray-400" />
+                <p class="font-serif text-md">
+                    Not a bad score for 5:34PM, rush hour! That's less than a
+                    minute for the most crowded part of the day!
+                </p>
+            </div>
+            <div class="flex flex-col max-w-xs">
+                <p class="text-green-600 font-display font-bold text-5xl">
+                    {trainsOnTime}
+                </p>
+                <p class="italic font-serif">trains on time</p>
+                <hr class="my-2 h-0.5 border-t-0 bg-gray-400" />
+                <p class="font-serif text-md">
+                    In your country, there's an average of 324 people on a
+                    train. This means 1.8 million people are transported to
+                    their destination without friction right now!
+                </p>
+            </div>
+            <div class="flex flex-col max-w-xs">
+                <p class="text-orange-600 font-display font-bold text-5xl">
+                    {Math.round(onTimePercentage * 10) / 10}%
+                </p>
+                <p class="italic font-serif">
+                    of trains aren't delayed right now
+                </p>
+                <hr class="my-2 h-0.5 border-t-0 bg-gray-400" />
+                <p class="font-serif text-md">
+                    Even for rush hour, this is not the best ever, but keep in
+                    mind most of these are small &lt;5 minute delays.
+                </p>
+            </div>
         </div>
-        <div class="flex flex-col max-w-xs">
-            <p class="text-orange-600 font-display font-bold text-5xl">33.1%</p>
-            <p class="italic font-serif">of trains aren't delayed right now</p>
-            <hr class="my-2 h-0.5 border-t-0 bg-gray-400" />
-            <p class="font-serif text-md">
-                Even for rush hour, this is not the best ever, but keep in mind
-                most of these are small &lt;5 minute delays.
-            </p>
-        </div>
-    </div>
+    {/if}
 </div>
